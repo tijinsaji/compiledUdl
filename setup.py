7f2f9b69c76683bffb3db72e3851f25ef24b6e89
tijinsaji <68032174+tijinsaji@users.noreply.github.com>
2026-06-03 16:17:28 +0530

    Rename package from 'compile-dUdl' to 'compiledUdl'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="compile-dUdl", 
+    name="compiledUdl", 
     version="0.1.0",
     author="Tijin Saji",
     author_email="tijinsaji97@gmail.com",
